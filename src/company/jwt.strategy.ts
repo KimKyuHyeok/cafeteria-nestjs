25a316613b09532382da439271d6da358837b7ec
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-12-09 19:07:44 +0900

    feat: jwt token 설정

---
+++ src/company/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { CompanyService } from "./company.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly companyService: CompanyService,
+        private configService: ConfigService
+        ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.get('JWT_ACCESS_SECRET')
+        });
+    }
+
+    async validate(payload: any) {
+        const user = await this.companyService.validateCompany(payload.userId);
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
