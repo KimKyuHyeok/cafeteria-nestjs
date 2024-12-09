230c9e656398378121782d21496d77e2769f844e
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-12-09 19:06:33 +0900

    feat: password 관련 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { AppResolver } from './app.resolver';
 import { ApolloDriver } from '@nestjs/apollo';
 import { CompanyModule } from './company/company.module';
+import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      debug: true,
       autoSchemaFile: true,
       driver: ApolloDriver
+    }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleware'),
+            logLevel: 'log',
+          })
+        ]
+      }
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
     }),
     CompanyModule,
   ],
