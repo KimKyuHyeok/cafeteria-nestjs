981a8f9e03988340ffc80a311a6fda9fadbed746
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-12-24 18:05:19 +0900

    feat: user.decorator 생성

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,10 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+export const UserEntity = createParamDecorator(
+    (data: unknown, ctx: ExecutionContext) => {
+      const user = GqlExecutionContext.create(ctx).getContext().req.user;
+      return user;
+    }
+  );
+  
