2fccd18ffcc7c2420bdb4f77837ad18d09d644fa
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-12-27 18:51:41 +0900

    feat: usage-history 기본 기능 생성

---
+++ src/usage-history/usage-history.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { UsageHistoryService } from './usage-history.service';
+
+@Module({
+  providers: [UsageHistoryService]
+})
+export class UsageHistoryModule {}
