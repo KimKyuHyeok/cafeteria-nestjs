29dc17488909fda773e57bd33ebfbc4cd46fe9cf
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-01-13 17:10:54 +0900

    feat: store 로그인/회원가입 mutation 작성

--- src/store/store.resolver.ts
+++ src/store/store.resolver.ts
@@ -1,4 +1,30 @@
-import { Resolver } from '@nestjs/graphql';
+import { Token } from 'src/common/auth/model/token.model';
+import { StoreInput } from './dto/store-input.dto';
+import { StoreService } from './store.service';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Auth } from 'src/company/model/auth.model';
 
 @Resolver()
-export class StoreResolver {}
+export class StoreResolver {
+    constructor(private readonly storeService: StoreService) {}
+
+    @Mutation(() => Auth)
+    async storeSignin(@Args('data') data: StoreInput): Promise<Token> {
+        const { accessToken, refreshToken } = await this.storeService.storeSignin(data);
+
+        return { 
+            accessToken,
+            refreshToken
+        }
+    }
+
+    @Mutation(() => Auth)
+    async storeSignup(@Args('data') data: StoreInput): Promise<Token> {
+        const { accessToken, refreshToken } = await this.storeService.storeSignup(data);
+
+        return { 
+            accessToken,
+            refreshToken
+        }
+    }
+}
