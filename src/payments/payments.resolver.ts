d48d267d7041de225b5303213ef11faa7573eecb
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-01-22 16:24:55 +0900

    feat: 결제내역 조회 기능 추가

--- src/payments/payments.resolver.ts
+++ src/payments/payments.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { PaymentsService } from './payments.service';
+import { GqlCompanyAuthGuard } from 'src/company/gql-company-auth.guard';
+import { Payments } from './model/payments.model';
+import { CompanyEntity } from 'src/common/decorators/company.decorator';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver()
-export class PaymentsResolver {}
+export class PaymentsResolver {
+    constructor(private readonly paymentsService: PaymentsService) {}
+
+    @UseGuards(GqlCompanyAuthGuard)
+    @Query(() => [Payments])
+    async selectPayments(@CompanyEntity() company: any): Promise<Payments[]> {
+        return this.paymentsService.selectPayments(company.id);
+    }
+}
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,20 @@
+import { PrismaService } from 'nestjs-prisma';
 import { Injectable } from '@nestjs/common';
+import { Payments } from './model/payments.model';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+    constructor(private readonly prisma: PrismaService){}
+
+    async selectPayments(id: number): Promise<Payments[]> {
+        try {
+            return await this.prisma.payments.findMany({
+                where: {
+                    companyId: id
+                }
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
