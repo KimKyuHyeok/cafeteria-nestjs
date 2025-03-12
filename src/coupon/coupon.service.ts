4d5227f34bed776135a502f2d836828dc5566d30
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-03-12 19:16:23 +0900

    fix: global exception filter 적용

--- src/coupon/coupon.service.ts
+++ src/coupon/coupon.service.ts
@@ -22,130 +22,126 @@ export class CouponService {
   async couponsFindByCompanyId(
     company: any,
   ): Promise<RestaurantWithCouponsDto[]> {
-    try {
-      const restaurantsWithCoupons = await this.prisma.restaurant.findMany({
-        where: {
-          coupon: {
-            some: {
-              companyId: company.id,
-              count: {
-                gte: 1,
-              },
+
+    const restaurantsWithCoupons = await this.prisma.restaurant.findMany({
+      where: {
+        coupon: {
+          some: {
+            companyId: company.id,
+            count: {
+              gte: 1,
             },
           },
         },
-        include: {
-          coupon: {
-            where: {
-              companyId: company.id,
-              count: {
-                gte: 1,
-              },
+      },
+      include: {
+        coupon: {
+          where: {
+            companyId: company.id,
+            count: {
+              gte: 1,
             },
-            select: {
-              id: true,
-              restaurantId: true,
-              companyId: true,
-              count: true,
-            },
+          },
+          select: {
+            id: true,
+            restaurantId: true,
+            companyId: true,
+            count: true,
           },
         },
-      });
+      },
+    });
 
-      const result = restaurantsWithCoupons.map((restaurant) => ({
-        id: restaurant.id,
-        name: restaurant.name,
-        address: restaurant.address,
-        coupon: restaurant.coupon.reduce(
-          (acc, curr) => {
-            const existing = acc.find(
-              (item) => item.restaurantId === curr.restaurantId,
-            );
-            if (existing) {
-              existing.count += curr.count; // count 합산
-            } else {
-              acc.push({ ...curr });
-            }
-            return acc;
-          },
-          [] as {
-            id: number;
-            restaurantId: number;
-            companyId: number;
-            count: number;
-          }[],
-        ),
-      }));
+    const result = restaurantsWithCoupons.map((restaurant) => ({
+      id: restaurant.id,
+      name: restaurant.name,
+      address: restaurant.address,
+      coupon: restaurant.coupon.reduce(
+        (acc, curr) => {
+          const existing = acc.find(
+            (item) => item.restaurantId === curr.restaurantId,
+          );
+          if (existing) {
+            existing.count += curr.count; // count 합산
+          } else {
+            acc.push({ ...curr });
+          }
+          return acc;
+        },
+        [] as {
+          id: number;
+          restaurantId: number;
+          companyId: number;
+          count: number;
+        }[],
+      ),
+    }));
 
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
+    return result;
+
   }
 
   async couponsFindByUserId(user: any): Promise<RestaurantWithCouponsDto[]> {
-    try {
-      const companyUser = await this.prisma.companyUser.findFirst({
-        where: { userId: user.id },
-      });
-      const restaurantsWithCoupons = await this.prisma.restaurant.findMany({
-        where: {
-          coupon: {
-            some: {
-              companyId: companyUser.companyId,
-              count: {
-                gte: 1,
-              },
+
+    const companyUser = await this.prisma.companyUser.findFirst({
+      where: { userId: user.id },
+    });
+    const restaurantsWithCoupons = await this.prisma.restaurant.findMany({
+      where: {
+        coupon: {
+          some: {
+            companyId: companyUser.companyId,
+            count: {
+              gte: 1,
             },
           },
         },
-        include: {
-          coupon: {
-            where: {
-              companyId: companyUser.companyId,
-              count: {
-                gte: 1,
-              },
+      },
+      include: {
+        coupon: {
+          where: {
+            companyId: companyUser.companyId,
+            count: {
+              gte: 1,
             },
-            select: {
-              id: true,
-              restaurantId: true,
-              companyId: true,
-              count: true,
-            },
+          },
+          select: {
+            id: true,
+            restaurantId: true,
+            companyId: true,
+            count: true,
           },
         },
-      });
+      },
+    });
 
-      const result = restaurantsWithCoupons.map((restaurant) => ({
-        id: restaurant.id,
-        name: restaurant.name,
-        address: restaurant.address,
-        coupon: restaurant.coupon.reduce(
-          (acc, curr) => {
-            const existing = acc.find(
-              (item) => item.restaurantId === curr.restaurantId,
-            );
-            if (existing) {
-              existing.count += curr.count; // count 합산
-            } else {
-              acc.push({ ...curr });
-            }
-            return acc;
-          },
-          [] as {
-            id: number;
-            restaurantId: number;
-            companyId: number;
-            count: number;
-          }[],
-        ),
-      }));
+    const result = restaurantsWithCoupons.map((restaurant) => ({
+      id: restaurant.id,
+      name: restaurant.name,
+      address: restaurant.address,
+      coupon: restaurant.coupon.reduce(
+        (acc, curr) => {
+          const existing = acc.find(
+            (item) => item.restaurantId === curr.restaurantId,
+          );
+          if (existing) {
+            existing.count += curr.count; // count 합산
+          } else {
+            acc.push({ ...curr });
+          }
+          return acc;
+        },
+        [] as {
+          id: number;
+          restaurantId: number;
+          companyId: number;
+          count: number;
+        }[],
+      ),
+    }));
 
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
+    return result;
+
   }
 
   // 잔여 쿠폰 확인
@@ -153,115 +149,100 @@ export class CouponService {
     company: any,
     data: CouponSelectDto,
   ): Promise<number> {
-    try {
-      const coupons = await this.prisma.coupon.findMany({
-        where: {
-          companyId: company.id,
-          restaurantId: data.restaurantId,
-        },
-        select: {
-          count: true,
-        },
-      });
 
-      const totalCount = coupons.reduce(
-        (sum, coupon) => sum + (coupon.count || 0),
-        0,
-      );
+    const coupons = await this.prisma.coupon.findMany({
+      where: {
+        companyId: company.id,
+        restaurantId: data.restaurantId,
+      },
+      select: {
+        count: true,
+      },
+    });
 
-      return totalCount;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+    const totalCount = coupons.reduce(
+      (sum, coupon) => sum + (coupon.count || 0),
+      0,
+    );
+
+    return totalCount;
   }
 
   // 쿠폰 충전
   async couponCharge(data: CouponChargeDto, company: any): Promise<Coupon> {
-    try {
-      if (company.id !== data.companyId)
-        throw new ForbiddenException('권한이 없습니다.');
 
-      return await this.prisma.coupon.create({
-        data: data,
-      });
-    } catch (error) {
-      throw new BadRequestException('에러가 발생하였습니다 : ', error);
-    }
+    if (company.id !== data.companyId)
+    throw new ForbiddenException('권한이 없습니다.');
+
+    return await this.prisma.coupon.create({
+      data: data,
+    });
   }
 
   // 잔여 쿠폰 확인 후 차감
-  async couponUse(data: QrDataDto): Promise<CouponResponse> {
-    try {
-      const coupon = await this.prisma.coupon.findFirst({
-        where: {
-          id: data.couponId,
-        },
-      });
+  async couponUse(data: QrDataDto, user: any): Promise<CouponResponse> {
 
-      if (coupon.count < 1)
-        throw new NotFoundException('잔여 식권 수량이 부족합니다.');
+    const coupon = await this.prisma.coupon.findFirst({
+      where: {
+        id: data.couponId,
+      },
+    });
 
-      await this.prisma.coupon.update({
-        where: { id: coupon.id },
-        data: { count: coupon.count - 1 },
-      });
+    if (coupon.count < 1)
+      throw new NotFoundException('잔여 식권 수량이 부족합니다.');
 
-      // await this.prisma.usageHistory.create({
-      //     data: {
-      //         coupon: { connect: { id: coupon.id }},
-      //         restaurant: { connect: { id: data.restaurantId }},
-      //         user: { connect: { id: data.userId }}
-      //     }
-      // })
+    await this.prisma.coupon.update({
+      where: { id: coupon.id },
+      data: { count: coupon.count - 1 },
+    });
 
-      return {
-        success: true,
-        message: '식권 사용이 완료되었습니다.',
-      };
-    } catch (error) {
-      return {
-        success: false,
-        message: error,
-      };
-    }
+    await this.prisma.usageHistory.create({
+        data: {
+            coupon: { connect: { id: coupon.id }},
+            restaurant: { connect: { id: coupon.restaurantId }},
+            user: { connect: { id: user.id }}
+        }
+    })
+
+    return {
+      success: true,
+      message: '식권 사용이 완료되었습니다.',
+    };
   }
 
   async generateQrCode(
     user: any,
     data: CouponUseDto,
   ): Promise<QRCodeResponseDto> {
-    try {
-      const companyUser = await this.prisma.companyUser.findFirst({
-        where: {
-          userId: user.id,
-          companyId: data.companyId,
-        },
-      });
-
-      if (!companyUser) throw new ForbiddenException('권한이 없습니다.');
-
-      const count = await this.prisma.coupon.findFirst({
-        where: {
-          companyId: data.companyId,
-          restaurantId: data.restaurantId,
-          count: { gte: 1 },
-        },
-      });
-
-      if (!count) throw new NotFoundException('쿠폰 잔여 수량이 부족합니다.');
-
-      const qrData = JSON.stringify({
-        couponId: count.id,
+    const companyUser = await this.prisma.companyUser.findFirst({
+      where: {
         userId: user.id,
-        restaurantId: data.restaurantId,
-      });
-      const qrCode = await QRCode.toDataURL(qrData);
-      const response = new QRCodeResponseDto();
-      response.url = qrCode;
+        companyId: data.companyId,
+      },
+    });
 
-      return response;
-    } catch (error) {
-      throw new Error(`QR 코드 생성 실패: ${error.message}`);
-    }
+    if (!companyUser) throw new ForbiddenException('권한이 없습니다.');
+
+    const count = await this.prisma.coupon.findFirst({
+      where: {
+        companyId: data.companyId,
+        restaurantId: data.restaurantId,
+        count: { gte: 1 },
+      },
+    });
+
+    if (!count) throw new NotFoundException('쿠폰 잔여 수량이 부족합니다.');
+
+    const qrData = JSON.stringify({
+      couponId: count.id,
+      userId: user.id,
+      restaurantId: data.restaurantId,
+    });
+    const qrCode = await QRCode.toDataURL(qrData);
+    const response = new QRCodeResponseDto();
+    response.url = qrCode;
+
+    return response;
+
   }
 }
