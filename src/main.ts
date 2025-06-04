68281fa021f235d10d58df3a5f21c0d490dcb94b
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-04 18:08:27 +0900

    fix: cors 설정 임시 비활성화 및 소켓어댑터 적용

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/exception/global-exception.filter';
+import { CustomSocketIoAdapter } from './common/adapter/websocket.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
 
-  app.enableCors({
-    origin: 'http://www.siggwon-moa.shop',
-    methods: 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: 'http://www.siggwon-moa.shop',
+  //   methods: 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
+
+  app.enableCors();
+
+  app.useWebSocketAdapter(new CustomSocketIoAdapter(app));
 
   await app.listen(process.env.PORT ?? 3000);
 }
