49c594262c6032f3e6cc6e9426003205b24954e2
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-06-10 16:35:44 +0900

    fix: 미들웨어 검증 및 구조 변경

--- src/common/middleware/socket.middleware.ts
+++ src/common/middleware/socket.middleware.ts
@@ -1,35 +1,31 @@
-import { Injectable, NestMiddleware, NotFoundException, UnauthorizedException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { Socket } from "socket.io";
-import { CustomSocket } from "../type/custom-socket";
-import { ChatService } from "src/chat/chat.service";
+import { JwtService } from '@nestjs/jwt';
+import { ChatService } from 'src/chat/chat.service';
+import { Socket } from 'socket.io';
+import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class SocketMiddleware implements NestMiddleware {
-    constructor(
-        private jwtService: JwtService,
-        private chatService: ChatService
-    ) {}
+export function createSocketMiddleware(
+	jwtService: JwtService,
+	chatService: ChatService,
+) {
+	return async (socket: Socket & { user?: any; company?: any }, next: (err?: any) => void) => {
+	const { userToken, companyToken } = socket.handshake.auth.token || {};
 
-    async use(socket: Socket & { user?: any }, next: (err?: any) => void) {
-        const client = socket as CustomSocket;
-        const { userToken, companyToken } = socket.handshake.auth.token;
+	try {
+		if (userToken) {
+			const payload = jwtService.verify(userToken);
+			const companyUser = await chatService.companyUserFindByUserId(payload);
+			socket.user = companyUser;
+		} else if (companyToken) {
+			const payload = jwtService.verify(companyToken);
+			socket.company = payload;
+		} else {
+			return next(new Error('No token provided'));
+		}
 
-        try {
-            if (userToken) {
-                const payload = this.jwtService.verify(userToken);
-                const companyUser = await this.chatService.companyUserFindByUserId(payload.sub);
-                client.user = companyUser;
-            } else if (companyToken) {
-                const payload = this.jwtService.verify(companyToken);
-                client.company = payload;
-            } else {
-                return next(new Error('No token provided'));
-            }
-
-            next();
-        } catch (error) {
-            return next(new Error('Invalid token'))
-        }
+		next();
+    } catch (err) {
+		console.error('socket middleware error : ', err);
+      	return next(new Error('Invalid token'));
     }
+  };
 }
